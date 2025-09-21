f039098f994b00e993372ba79e51d6f7e2959230
Lucca Serafim <lucca.serafim@gmail.com>
2025-09-21 14:24:26 -0300

    Removendo do github

--- Esqueleto-Inicial/Visualizador.py
+++
@@ -1,9 +0,0 @@
-import osmnx as ox
-
-class Visualizador:
-    @staticmethod
-    def plotar_grafo(grafo):
-        if grafo is not None:
-            ox.plot_graph(grafo)
-        else:
-            print("Nenhum grafo para exibir.")
